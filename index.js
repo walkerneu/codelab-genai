132fc9878f4d4e9a8dd464301a5f022964db3a35
walkerneu <walkerneudorff@gmail.com>
2024-07-30 18:15:26 +0000

    updated app with vertex ai

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
-
 const app = express();
+
+import { VertexAI } from '@google-cloud/vertexai';
+
+import { GoogleAuth } from 'google-auth-library';
+const auth = new GoogleAuth();
+
 app.get('/', async (req, res) => {
-    res.send('Hello world!');
+    const project = await auth.getProjectId();
+
+    const vertex = new VertexAI({ project: project });
+    const generativeModel = vertex.getGenerativeModel({
+        model: 'gemini-1.5-flash'
+    });
+
+    const animal = req.query.animal || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const resp = await generativeModel.generateContent(prompt);
+    const html = resp.response.candidates[0].content.parts[0].text;
+    res.send(html);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
